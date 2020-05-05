867e796e2cb6fe00b130c9d541fd0227b5483080
Sam Caldwell <mail@samcaldwell.net>
2020-05-04 20:43:31 -0500

    Fixes for build pipeline

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,14 +5,16 @@
 
 echo "Current Working Directory: $(pwd)"
 
+export GOPATH=$(pwd)/git
+echo "Export GOPATH"
+echo "--> GOPATH: $GOPATH"
+
+mkdir -p git/sam-caldwell
+cd git/sam-caldwell
 git clone git@github.com:sam-caldwell/adrestia-assertions.git
 git checkout development
 cd adrestia-assertions
 
-echo "Export GOPATH"
-export GOPATH=$(pwd)/adrestia-assertions
-echo "--> GOPATH: $GOPATH"
-
 echo "Run Go test"
-go test -v
+go test -a -v -test.benchmem -test.parallel 2
 echo "done"
