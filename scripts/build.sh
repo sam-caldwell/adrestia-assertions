f0e269dd36e0a56819c2f6848e7456316a7b047f
Sam Caldwell <mail@samcaldwell.net>
2020-05-04 20:51:00 -0500

    working on pipeline

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,10 +10,13 @@ echo "Export GOPATH"
 echo "--> GOPATH: $GOPATH"
 
 mkdir -p git/sam-caldwell
-cd git/sam-caldwell
+cd git/sam-caldwell || exit 1
+
 git clone git@github.com:sam-caldwell/adrestia-assertions.git
+
+cd adrestia-assertions || exit 1
 git checkout development
-cd adrestia-assertions
+git checkout $(git log -n1 | head -n1 | awk '{print $2}') -b build
 
 echo "Run Go test"
 go test -a -v -test.benchmem -test.parallel 2
