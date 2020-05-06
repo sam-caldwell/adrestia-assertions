df07c940a1eedbb24e08ca8e96b69ba3de550060
Sam Caldwell <mail@samcaldwell.net>
2020-05-06 11:39:10 -0500

    fix typo in automation scripts.

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -1,24 +1,28 @@
 #!/bin/bash -e
 
+export GOPATH="$GOPATH:$(pwd)/"
+
 echo "GOPATH: $GOPATH"
-
-echo "Install yamllint"
-brew install yamllint
-
-echo "Install golint"
-go get -u golang.org/x/lint/golint
 
 echo "Install git-hooks"
 export GO111MODULE="on"
 go get -u github.com/git-hooks/git-hooks
 
 echo "Install the git hooks to the local environment."
-git hooks install
+git hooks install  || {
+    ls -la
+}
 
 echo "List current hooks"
 git hooks list
 
 echo "Install GoDoc"
 go get golang.org/x/tools/cmd/godoc
+
+echo "Install yamllint"
+brew install yamllint
+
+echo "Install golint"
+go get -u golang.org/x/lint/golint
 
 echo "$0 done"
