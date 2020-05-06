5dfc7d011e1094f7266b91f9ca0447b0ecd60cef
Sam Caldwell <mail@samcaldwell.net>
2020-05-06 11:56:39 -0500

    fix typo in automation scripts.

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -9,6 +9,9 @@ echo "Install yamllint"
 brew install yamllint
 
 echo "Install golint"
-go get -u golang.org/x/lint/golint
-
+go get -u golang.org/x/lint/golint || {
+    echo "HOME: $HOME"
+    ls -la $HOME
+    exit 1
+}
 echo "$0 done"
