29ae9ff0ae44b45eb3fed343544a2ea8fea090a9
Sam Caldwell <mail@samcaldwell.net>
2020-05-06 12:57:37 -0500

    Refactor to fix automation

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -5,11 +5,14 @@ export GOPATH
 
 echo "Install GoLint"
 go get -u golang.org/x/lint/golint
+command -v golint || exit 1
 
 echo "Install GoDoc"
 go get golang.org/x/tools/cmd/godoc
+command -v godoc || exit 1
 
 echo "Install yamllint"
 brew install yamllint
+command -v yamllint || exit 1
 
 echo "$0 done"
