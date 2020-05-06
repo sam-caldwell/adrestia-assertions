73d502052631a25157af0a04f94803de2c28958b
Sam Caldwell <mail@samcaldwell.net>
2020-05-06 11:50:14 -0500

    fix typo in automation scripts.

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -4,25 +4,6 @@ export GOPATH="$GOPATH:$(pwd)/"
 
 echo "GOPATH: $GOPATH"
 
-echo "Install git-hooks"
-
-#go get -u github.com/git-hooks/git-hooks
-
-echo "Install the git hooks to the local environment."
-(
-    mkdir -p $GOPATH/src/github.com/git-hooks
-    cd $GOPATH/src/github.com/git-hooks
-    git clone https://github.com/git-hooks/git-hooks.git
-    cd git-hooks/
-    mkdir build
-    go build -o build/git-hooks
-    go install
-)
-git hooks install
-
-echo "List current hooks"
-git hooks list
-
 echo "Install GoDoc"
 go get golang.org/x/tools/cmd/godoc
 
