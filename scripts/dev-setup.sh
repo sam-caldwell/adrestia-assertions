0b1f8a4e808908f82fc2f1b3292f5c8590c4b46e
Sam Caldwell <mail@samcaldwell.net>
2020-05-05 22:28:35 -0500

    working on azure

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 export GO111MODULE="on"
+
+echo "Install git-hooks"
 go get -u github.com/git-hooks/git-hooks
 
+echo "Install the git hooks to the local environment."
 git hooks install
+
+echo "List current hooks"
 git hooks list
+echo "$0 done"
